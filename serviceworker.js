0c35b31f6e387a55aff5548ecd0a663e01ac73f7
Adrian Gonzalez <adrgon@gmail.com>
2021-12-13 19:24:00 -0300

    Versionado de cache

--- serviceworker.js
+++ serviceworker.js
@@ -1,34 +1,59 @@
 // SW Chárlalo v.2
 
-const CACHE_NAME = "charlalo";
-const urlsToCache = ["/", "styles.css", "scripts/camera.js", "scripts/charlalo.js", "scripts/recognition.js", 
-    "scripts/synthesis.js", "/pictogramas.json", "imagenes/charlalo_titulo.svg", "imagenes/icono.png"];
+const CACHE_NAME = "charlalo-v3.0";
+const urlsToCache = [
+  "/",
+  "styles.css",
+  "scripts/camera.js",
+  "scripts/charlalo.js",
+  "scripts/recognition.js",
+  "scripts/synthesis.js",
+  "/pictogramas.json",
+  "imagenes/charlalo_titulo.svg",
+  "imagenes/icono.png",
+];
 
-self.addEventListener("install", event => {
-    // Pre-cache archivos para offline y performance; los pictogramas los dejamos para la primera carga
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            return cache.addAll(urlsToCache).then( async () => {
-                const response = await fetch("/pictogramas.json");
-                const json = await response.json();
-                const pictogramas = json.map(p => "/imagenes/pictogramas/" + p.archivo);
-                return cache.addAll(pictogramas);
-            });
-        })
-    );
-});
-
-// Cache First Policy
-self.addEventListener("fetch", event => {
-  event.respondWith(
-      caches.match(event.request)  
-          .then(function(response) {
-              if (response) {  // CACHE HIT
-                  return response;
-              } else {    // CACHE MISS
-                  return fetch(event.request);
-              }
-          })
+self.addEventListener("install", (event) => {
+  // Pre-cache archivos para offline y performance; los pictogramas los dejamos para la primera carga
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache).then(async () => {
+        const response = await fetch("/pictogramas.json");
+        const json = await response.json();
+        const pictogramas = json.map(
+          (p) => "/imagenes/pictogramas/" + p.archivo
+        );
+        return cache.addAll(pictogramas);
+      });
+    })
   );
 });
 
+// Cache First Policy
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      if (response) {
+        // CACHE HIT
+        return response;
+      } else {
+        // CACHE MISS
+        return fetch(event.request);
+      }
+    })
+  );
+});
+
+self.addEventListener("activate", async () => {
+  const keys = await caches.keys();
+
+  await Promise.all(
+    keys.map((key) => {
+      if (key !== CACHE_NAME) {
+        return caches.delete(key);
+      }
+    })
+  );
+
+  clients.claim();
+});
